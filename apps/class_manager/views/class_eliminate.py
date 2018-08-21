dbc29705c754320a146026476d710e0fbc577f86
swlim <swlim@swlimdeMacBook-Pro.local>
2018-08-21 16:46:07 +0800

    add teacher period ok

--- apps/class_manager/views/class_eliminate.py
+++ apps/class_manager/views/class_eliminate.py
@@ -121,49 +121,66 @@ def class_eliminate(request):
 
                     return JsonResponse(public_methods.response_message(result, response, result_code))
 
+            commit_status = post["status"]
+            commit_id = eval(post["classInfo"])["id"]
             student_list = eval(post["studentLeftPeriod"])
             class_id = eval(post["classInfo"])["class_id"]
             class_name = eval(post["classInfo"])["class_name"]
-            teacher_id = eval(post["classInfo"])["teacher_id"]
-            teacher_name = eval(post["classInfo"])["teacher_name"]
+            teacher_id = eval(post["classInfo"])["user_id"]
+            teacher_name = eval(post["classInfo"])["name"]
             status = 1
             period_time = int(time.time())
             period_data = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            class_time = eval(post["classInfo"])["class_start_time"] + " - " + eval(post["classInfo"])["class_end_time"]
-            class_week = public_methods.week(eval(post["classInfo"])["class_week"])
-            class_time_info = class_week + "：" + class_time
+            # class_time = eval(post["classInfo"])["class_start_time"]+" - " + eval(post["classInfo"])["class_end_time"]
+            # class_week = public_methods.week(eval(post["classInfo"])["class_week"])
+            # class_time_info = class_week + "：" + class_time
+            class_time_info = eval(post["classInfo"])["class_time"]
             student_name_list = []
 
-            for i in range(len(student_list)):
-                student_name_list.append(student_list[i]["name"])
-                class_period_student_add = mysql_db.ClassPeriodStudent(
+            update_teacher_left_period = role_db.Teacher.objects.get(user_id=teacher_id)
+            update_teacher_left_period.left_commit_periods = int(update_teacher_left_period.left_commit_periods) - 1
+            update_teacher_left_period.save()
+
+            update_period_teacher = mysql_db.ClassPeriodTeacherSelf.objects.get(id=commit_id)
+
+            if commit_status == "1":
+                update_period_teacher.status = 1
+
+                for i in range(len(student_list)):
+                    student_name_list.append(student_list[i]["name"])
+                    class_period_student_add = mysql_db.ClassPeriodStudent(
+                        class_id=class_id,
+                        class_name=class_name,
+                        user_id=student_list[i]["userId"],
+                        name=student_list[i]["name"],
+                        status=1,
+                        period_time=period_time,
+                        period_data=period_data,
+                        class_time=class_time_info,
+                        class_teacher=teacher_name
+                    )
+                    class_period_student_add.save()
+                    update_student_period = role_db.Student.objects.get(user_id=student_list[i]["userId"])
+                    update_student_period.left_periods = int(update_student_period.left_periods) - 1
+                    update_student_period.save()
+
+                class_period_teacher_add = mysql_db.ClassPeriodTeacher(
                     class_id=class_id,
                     class_name=class_name,
-                    user_id=student_list[i]["userId"],
-                    name=student_list[i]["name"],
-                    status=1,
-                    period_time=period_time,
+                    user_id=teacher_id,
+                    name=teacher_name,
+                    status=status,
                     period_data=period_data,
+                    period_time=period_time,
                     class_time=class_time_info,
-                    class_teacher=teacher_name
+                    class_student=student_name_list
                 )
-                # class_period_student_add.save()
-                update_student_period = role_db.Student.objects.get(user_id=student_list[i]["userId"])
-                update_student_period.left_periods = int(update_student_period.left_periods) - 1
-                # update_student_period.save()
+                class_period_teacher_add.save()
 
-            class_period_teacher_add = mysql_db.ClassPeriodTeacher(
-                class_id=class_id,
-                class_name=class_name,
-                user_id=teacher_id,
-                name=teacher_name,
-                status=status,
-                period_data=period_data,
-                period_time=period_time,
-                class_time=class_time_info,
-                class_student=student_name_list
-            )
-            # class_period_teacher_add.save()
+            if commit_status == "2":
+                update_period_teacher.status = 2
+
+            update_period_teacher.save()
 
             response = {"message": "成功"}
             result = "success"
