b04848f046235f55a21bafb81a9f57ab6e50408e
swlim <swlim@swlimdeMacBook-Pro.local>
2018-08-21 17:00:20 +0800

    add teacher period ok

--- MysSchool/settings.py
+++ MysSchool/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'MysSchool.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'mys_school',
-#         'USER': 'root',
-#         'PASSWORD': 'MysSchool@2018',
-#         'HOST': '139.199.127.192',
-#         'PORT': '3306',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mys_school',
+        'USER': 'root',
+        'PASSWORD': 'MysSchool@2018',
+        'HOST': '139.199.127.192',
+        'PORT': '3306',
+    }
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
@@ -96,16 +96,16 @@ WSGI_APPLICATION = 'MysSchool.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mys_school',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'mys_school',
+#         'USER': 'root',
+#         'PASSWORD': '123456',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
