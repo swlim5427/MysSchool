422cfaad4829e8585e134b0fa7fb09a589933e9e
swlim <swlim@swlimdeMacBook-Pro.local>
2018-08-28 16:50:20 +0800

    1.0.2 fix teacher edit bug

--- MysSchool/settings.py
+++ MysSchool/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'MysSchool.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'mys_school',
-        'NAME': 'test',
+        'NAME': 'mys_school',
+        # 'NAME': 'test',
         'USER': 'root',
         'PASSWORD': 'MysSchool@2018',
         'HOST': '139.199.127.192',
